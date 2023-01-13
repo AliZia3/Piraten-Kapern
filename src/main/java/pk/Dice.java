364ee4cd2e65c51b9193d96afbb13dddbba36bf2
AliZia3 <alizia0217@gmail.com>
2023-01-12 22:05:58 -0500

    Implemented feature to roll 8 dice. Accomplished by returning an array of 8 rolls

--- src/main/java/pk/Dice.java
+++ src/main/java/pk/Dice.java
@@ -1,15 +1,21 @@
 package pk;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Dice {
 
-    public Faces roll() {
+    public ArrayList<Faces> roll() {
+        ArrayList<Faces> roll_results = new ArrayList<Faces>();
         int howManyFaces = Faces.values().length;
-        System.out.println("  (DEBUG) there are " + howManyFaces + " faces");
-        System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
+//        System.out.println("  (DEBUG) there are " + howManyFaces + " faces");
+//        System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
         Random bag = new Random();
-        return Faces.values()[bag.nextInt(howManyFaces)];
+
+        for(int i=0; i<7; i++){
+            roll_results.add(Faces.values()[bag.nextInt(howManyFaces)]);
+        }
+        return roll_results;
     }
     
 }
