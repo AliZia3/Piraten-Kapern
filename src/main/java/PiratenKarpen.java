3142e99095213ddf31e04b1955272882f7625931
AliZia3 <alizia0217@gmail.com>
2023-01-15 23:07:59 -0500

    Implemented feature to count number if skulls

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,3 +1,4 @@
+import pk.CheckSkulls;
 import pk.Dice;
 import pk.Faces;
 
@@ -11,8 +12,13 @@ public class PiratenKarpen {
         System.out.println("I'm rolling a dice");
 
         Dice myDice = new Dice();
+        CheckSkulls skulls = new CheckSkulls();
+
+        ArrayList<Faces> roll_results = myDice.rollEight();
+
         System.out.println(myDice.roll());
         System.out.println(myDice.rollEight());
+        System.out.println(skulls.checkSkulls(roll_results));
         
 
         System.out.println("That's all folks!");
---
+++ src/main/java/pk/CheckSkulls.java
@@ -0,0 +1,15 @@
+package pk;
+
+import java.util.ArrayList;
+
+public class CheckSkulls {
+    public int checkSkulls(ArrayList<Faces> roll_results) {
+        int skullCount = 0;
+        for (int i = 0; i < roll_results.size(); i++) {
+            if (roll_results.get(i) == Faces.SKULL) {
+                skullCount++;
+            }
+        }
+        return skullCount;
+    }
+}
