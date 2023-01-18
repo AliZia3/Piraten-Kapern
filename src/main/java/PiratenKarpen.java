1628dc7d86e7db39603abec36e6843eb3642cbc9
AliZia3 <alizia0217@gmail.com>
2023-01-17 19:50:34 -0500

    Completed feature F06 (Turn-based Functionality)

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,6 +1,8 @@
 import pk.CheckSkulls;
 import pk.Dice;
 import pk.Faces;
+import pk.ScorePoints;
+import pk.Turns;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,23 +11,34 @@ public class PiratenKarpen {
 
     public static void main(String[] args) {
         // Initial Vars
-        String player1 = "Player1";
-        String player2 = "Player1";
-        String currPlayer = player1;
-        boolean gameOver = false;
+        int gamesCount = 42;
+        int player1Wins = 0;
+        int player2Wins = 0;
 
-        // Object Instantiation
-        Dice myDice = new Dice();
+        String player1 = "Player 1";
+        String player2 = "Player 2";
 
         System.out.println("Welcome to Piraten Karpen Simulator!");
-        System.out.println("I'm rolling a dice");
 
-        // Initial roll
-        ArrayList<Faces> initialRoll = myDice.rollEight();
-        ArrayList<Faces> newRoll = myDice.nextRoll(initialRoll);
 
-        System.out.println("Initial Roll: " + initialRoll);
-        System.out.println("New Roll: " + newRoll);
+        int player1Score = 0;
+        int player2Score = 0;
+
+        while (player1Score <= 6000 && player2Score <= 6000) {
+            player1Score += Turns.turn(player1);
+            player2Score += Turns.turn(player2);
+        }
+
+        if (player1Score >= 6000) {
+            player1Wins++;
+        } else {
+            player2Wins++;
+        }
+
+        System.out.println("Player 1 Wins: " + player1Wins);
+        System.out.println("Player 2 Wins: " + player2Wins);
+
+
 
         System.out.println("That's all folks!");
     }
