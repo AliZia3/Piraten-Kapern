cd35334d540046db3e5ee993aeec11a04025b91a
AliZia3 <alizia0217@gmail.com>
2023-01-23 13:33:24 -0500

    Completed OOP for random strategy

--- src/main/java/pk/PlayerStrategies.java
+++ src/main/java/pk/PlayerStrategies.java
@@ -1,10 +1,33 @@
 package pk;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PlayerStrategies {
-    // public static int randomStrategy(String currPlayer) {
-    //     ArrayList<Faces> rollResults = Dice.rollEight();
-    //     int[] turnStatus = { DiceData.Skulls(rollResults), DiceData.pointsCoinsDiamonds(rollResults) };
-    // }
+    public static int randomStrategy(String currPlayer) {
+        ArrayList<Faces> rollResults = Dice.rollEight();
+        int[] turnStatus = { DiceData.Skulls(rollResults), DiceData.pointsCoinsDiamonds(rollResults) };
+
+        if (turnStatus[0] >= 3) {
+            return 0;
+        } else {
+            Random attempts = new Random();
+            int turnCondition = attempts.nextInt(2);
+
+            while (turnCondition != 0) {
+                ArrayList<Faces> nextRollResults = Dice.reRoll(rollResults);
+
+                turnStatus[0] = DiceData.Skulls(nextRollResults);
+                turnStatus[1] = DiceData.pointsCoinsDiamonds(nextRollResults);
+
+                if (turnStatus[0] >= 3) {
+                    return 0;
+                } else {
+                    turnCondition = attempts.nextInt(2);
+                }
+
+            }
+            return turnStatus[1];
+        }
+    }
 }
