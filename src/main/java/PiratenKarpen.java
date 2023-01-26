19197bbf7f4b8ab199fa3c386ca5bebf8761fdf8
AliZia3 <alizia0217@gmail.com>
2023-01-26 17:20:42 -0500

    Completed feature to let users decide between strategies and trace mode

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -6,8 +6,21 @@ public class PiratenKarpen {
     public static void main(String[] args) {
 
         Game game = new Game();
-        game.play(42,true, "random", "combo");
-
+        // If no command line arguments given
+        if (args.length == 0) {
+            game.play(42, false, "random", "random");
+        }
+        // Run without trace mode and with strategies ('trace' not given as command line argument)
+        else if (args.length < 3) {
+            game.play(42,false, args[0], args[1]);
+        }
+        // Run with trace mode and with strategies ('trace' given as command line argument)
+        else {
+            for (String arg:args) {
+                game.play(42, arg.contains("trace"), args[1], args[2]);
+                break;
+            }
+        }
 
     }
 }
